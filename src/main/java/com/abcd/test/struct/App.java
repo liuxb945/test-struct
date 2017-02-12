9a23073f3e5ceff211cc4c39827ba803d8692134
liuxb945 <280445715@qq.com>
2017-02-12 18:16:36 +0800

    dd

---
+++ src/main/java/com/abcd/test/struct/App.java
@@ -0,0 +1,13 @@
+package com.abcd.test.struct;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
